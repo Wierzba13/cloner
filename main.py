ed8614549ec16a47478cc47b954f0e1644e30326
Raul Wierzbinski <Wierzba@tuta.io>
2021-05-03 11:47:55 +0200

    Edit help page and repair flags behavior

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--repo")
-parser.add_argument("--clone", nargs = "*")
-parser.add_argument("--output")
+parser.add_argument("--repo", required=True, help="URL of repository")
+parser.add_argument("--output", help="Directory for clone files and directories")
+parser.add_argument("--clone", nargs = "*", help="List of files and directories to clone")
 args = parser.parse_args()
 
 repo = args.repo
-outDir = args.output
+outDir = args.output if args.output else os.getcwd()
 dirs = args.clone
 
 if(os.path.exists(outDir)):
@@ -18,9 +18,11 @@ if(os.path.exists(outDir)):
     os.system("git init")
     os.system(f"git remote add -f origin {repo}")
     os.system("git config core.sparseCheckout true")
-    for item in dirs:
-        os.system(f"echo {item} >> .git/info/sparse-checkout")
-        print(item)
+    if(dirs): 
+        for item in dirs:
+            os.system(f"echo {item} >> .git/info/sparse-checkout")
+            print(item)
+
     os.system("git pull origin main")
     os.system("rm -rf .git/")
 
